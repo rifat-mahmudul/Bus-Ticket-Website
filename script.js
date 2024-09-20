353afb5df1f7208662f51ca46f483bd4d932aaa8
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-09-20 15:31:37 +0600

    increase and decrease seat number

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const seats = document.getElementsByClassName('seats');
 const bookingSeat = document.getElementById('booking-seat');
 const couponInput = document.getElementById('couponInput');
 const cuponApplyBtn = document.getElementById('cuponApplyBtn');
+const seatCount = document.getElementById('seatCount');
+const leftSeatCount = document.getElementById('leftSeatCount');
+
+let totalSeat = 40;
+let selectedSeat = [];
 
 for(let seat of seats){
     seat.addEventListener('click', (event) =>{
         
-        seat.style.backgroundColor = '#1DD100'
+        seat.style.backgroundColor = '#1DD100';
+        seat.style.color = 'white';
+
+        selectedSeat.push(event.target.innerText);
 
         const seatValue = event.target.innerText;
         bookingSeat.innerHTML = `
@@ -18,6 +26,12 @@ for(let seat of seats){
                 </li>
             </ul>
         `
+
+        
+
+        // increase and decrease available seat
+        seatCount.innerText = selectedSeat.length;
+        leftSeatCount.innerText = totalSeat - selectedSeat.length;
 
     })
 }
